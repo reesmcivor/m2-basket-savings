a38f4be89c4585911d6af9659af1283cce1bcf57
reesmcivor <reesmcivor@hotmail.com>
2023-07-27 11:40:29 +0100

    Basket savings

---
+++ Block/SavingsTotal.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ReesMcIvor\BasketSavings\Block;
+
+class SavingsTotal extends \Magento\Framework\View\Element\Template
+{
+    public function getCustomBlockContent()
+    {
+        return "This is my custom block!";
+    }
+}
