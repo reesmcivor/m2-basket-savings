d89ff7fe7c64b53070975f5442b11d2dbc6a6724
reesmcivor <reesmcivor@hotmail.com>
2023-07-27 13:54:45 +0100

    Block savings

---
+++ Block/Savings.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace ReesMcIvor\BasketSavings\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Checkout\Model\Cart;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Directory\Model\Currency;
+
+class Savings extends Template
+{
+    protected $cart;
+    protected $storeManager;
+    protected $currency;
+
+    public function __construct(
+        Template\Context $context,
+        Cart $cart,
+        StoreManagerInterface $storeManager,
+        Currency $currency,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->cart = $cart;
+        $this->storeManager = $storeManager;
+        $this->currency = $currency;
+    }
+
+    public function getCartItems()
+    {
+        return $this->cart->getQuote()->getAllItems();
+    }
+
+    public function calculateSavings($item)
+    {
+        $originalPrice = $item->getProduct()->getPrice();
+        $salePrice = $item->getPriceInclTax();
+        if($salePrice < $originalPrice) {
+            return $originalPrice - $salePrice;
+        }
+        return 0;
+    }
+
+    public function calculateTotalSavings()
+    {
+        $totalSavings = 0;
+        $totalOriginalPrice = 0;
+
+        $items = $this->getCartItems();
+        foreach ($items as $item)
+        {
+            if($item->getTypeId() != "bundle") {
+                $totalSavings += $this->calculateSavings($item);
+                $totalOriginalPrice += $item->getProduct()->getPrice() * $item->getQty();
+            }
+
+        }
+
+        $quote = $this->cart->getQuote();
+        $discountAmount = $quote->getSubtotal() - $quote->getSubtotalWithDiscount();
+        $totalSavings += $discountAmount;
+
+        return [
+            'total_original_price' => $totalOriginalPrice,
+            'total_savings' => $totalSavings,
+        ];
+    }
+
+    public function getCurrencySymbol()
+    {
+        $currencyCode = $this->storeManager->getStore()->getCurrentCurrencyCode();
+        $currencySymbol = $this->currency->load($currencyCode)->getCurrencySymbol();
+
+        return $currencySymbol;
+    }
+}
